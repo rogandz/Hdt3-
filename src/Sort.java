9d5a983dba2a5f469e1b0f1cd40cd7fd00e620d7
rogandz <andrezz.giron@gmail.com>
2016-07-29 11:21:08 -0600

    Estos eran los archivos correctos
    Se borro la clase Random y se implemento en el main

--- src/Sort.java
+++ src/Sort.java
@@ -1,19 +1,33 @@
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-public class Sort {
-	Random aleatorio = new Random();
-	private static int[] arreglo;
-	
-	
-	public void llenarArreglo(){
-		for(int x = 3000; x> 0; x--){
-			arreglo[x] = aleatorio.rands();
+/*UNIVERSIDAD DEL VALLE DE GUATEMALA / ADT SECCION 30 
+ * Integrantes: 
+ *-Paulina Cano 15053 
+ *-Dieter De Wit 15 
+ *-Brandon Hernandez 15326
+ *-Andres Giron 15174 
+ */
 
+import java.io.*;
+
+public class Sort {
+	private int[] arreglo;
+	
+	
+	public void llenarArreglo(int num,int cont){
+		try {
+			arreglo = new int[3000];
+			FileWriter guardar= new FileWriter("Datos.txt");
+			PrintWriter escribir= new PrintWriter(guardar);
+			arreglo[cont] = num;
+			escribir.println(arreglo[cont]);
+			System.out.println(num);
+			guardar.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public void guardarTxt(){
-		for(int x = 3000; x>0; x--){
+	public void guardarTxt(int x){
+		for(int i = 3000; x>0; x--){
 			try{
 				
 			}
