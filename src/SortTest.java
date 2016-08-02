5a0626fa5e54909e2c296df43f350a370a75f6c2
Her15326 <Her15326@uvg.edu.gt>
2016-08-01 23:07:17 -0600

    Aqui se implemento un jUnit para el quickSorting
    Aquí esta ya implementado.

--- src/SortTest.java
+++ src/SortTest.java
@@ -10,11 +10,17 @@ public class SortTest {
 	     Sort instance = new Sort();
 	     String expResult = instance.leerText("numeros.txt") ;
 	     int[] result = instance.leerDatos(expResult);
+	     for(int i = 0 ; i < 8; i++)
+	     {
+	    	 System.out.println("Antes de hacer sort... " + result[i]);
+	     }
 	     result = instance.quickSorting(result, 0, 8-1);
 	     String resultado = "";
+	   
 	     for(int i = 0 ; i < 8; i++)
 	     {
 	    	 resultado = resultado + String.valueOf(result[i]);
+	    	 System.out.println("pruebas con el sort... " + result[i]);
 	     }
 	     assertEquals(resultado,"12345679",true);
 		 fail("Not yet implemented");
